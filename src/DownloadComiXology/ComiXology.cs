cc1fd0a4385e512cfaeb39c4a1e80307a541aed3
Oren Nachman <git@nachmore.com>
2013-04-12 14:52:29 -0700

    Initial dump of downloader (functional)

---
+++ src/DownloadComiXology/ComiXology.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WatiN.Core;
+using WatiN.Core.Exceptions;
+
+namespace DownloadComiXology
+{
+    class ComiXology
+    {
+        public string Url { get; private set; }
+        public string Password { get; private set; }
+
+        public ComiXology(string url, string password)
+        {
+            Url = url;
+            Password = password;
+        }
+
+        ~ComiXology() 
+        {
+            if (_browser != null)
+                _browser.Dispose();
+        }
+
+        IE _browser;
+
+        public void Run()
+        {
+            _browser = new IE(Url);
+
+            Login();
+
+            // iterate through all sections
+            var i = 0;
+
+            while (true)
+            {
+                i++;
+
+                if (!PurchaseCompleteSection(i)) 
+                {
+                    // we're done!
+                    break;
+                }
+            }
+       }
+
+        private bool PurchaseCompleteSection(int index)
+        {
+            // cowboy style!
+            _browser.WaitForComplete();
+            Thread.Sleep(1000);
+
+            while (true)
+            {
+                // get the current section
+                // we reget it every time due to the changing HTML
+                Div section;
+
+                try
+                {
+                    section = _browser.Divs.Where(elem => elem.Id == "ahd" + index + "_container").First();
+                }
+                catch (InvalidOperationException)
+                {
+                    // i.e. we couldn't find the element, so the Where returned 0 items, and then threw on First()
+                    return false;
+                }
+
+                var rows = section.ChildrenOfType<List>();
+
+                foreach (var row in rows)
+                {
+                    foreach (var item in row.ChildrenOfType<ListItem>())
+                    {
+                        var link = item.Links[0];
+
+                        // we've already been here, we're at the end of the section, progress to the next one
+                        if (link.Text.Contains("In Cart"))
+                            return true;
+                        
+                        link.Click();
+                    }
+                }
+
+                // click the "Next" button
+                try
+                {
+                    section.Link(index + "_AdHocNext").Click();
+                }
+                catch (ElementNotFoundException)
+                {
+                    // nothing to click, no next page of comics for this section, move to the next section
+                    return true;
+                }
+
+                _browser.WaitForComplete();
+                Thread.Sleep(3000);
+            }
+
+        }
+
+        private void GoToNextSectionPage(int id)
+        {
+            _browser.Link("19_AdHocNext").Click();
+        }
+
+        private void Login()
+        {
+            var passwordBox = _browser.TextField(Find.ByName("password"));
+            passwordBox.TypeText(Password);
+            passwordBox.KeyPress('\r');
+            System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+        }
+    }
+}
---
+++ src/DownloadComiXology/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WatiN.Core;
+
+namespace DownloadComiXology
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ComiXology _c;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _c = new ComiXology(txtUrl.Text, txtPassword.Text);
+            _c.Run();
+
+            MessageBox.Show("Finished adding to cart - you can now check out in the browser window\n\nBEWARE: Closing this application first might close your browser window!", "All Done!", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
